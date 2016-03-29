13f5dff5512e5f94091e7376d88df759f3d3ec99
BaerMitUmlaut <florian.foss@gmail.com>
2016-03-29 04:31:10 +0200

    Adjusted hitmarker fadeout

--- addons/hitmarkers/RscTitles.hpp
+++ addons/hitmarkers/RscTitles.hpp
@@ -4,8 +4,8 @@ class RscTitles {
         movingEnable = 0;
         enableSimulation = 1;
         fadein = 0;
-        fadeout = 0.1;
-        duration = 0.1;
+        fadeout = 0.2;
+        duration = 0.2;
 
         controlsBackground[] = {CtrlHitmarker};
         controls[] = {};
