56c2a1c8481a9c5566b4d459eb3becedf3d24f4b
bux <github@jonathandavid.de>
2016-04-01 10:34:01 +0200

    add picture and displaynameshort for grenade

--- addons/grenade/CfgMagazines.hpp
+++ addons/grenade/CfgMagazines.hpp
@@ -3,9 +3,11 @@ class CfgMagazines {
 
     class GVAR(bananaGrenade): HandGrenade {
         displayName = "Banana Grenade";
+        displayNameShort = "BG 1416";
         descriptionShort = "A powerful banana grenade";
         author = "ABE3";
         ammo = "GrenadeBanana";
         model = QUOTE(PATHTOEF(banana,data\banana_lp.p3d));
+        picture = QUOTE(PATHTOF(data\grenade_banana_pic.paa));
     };
 };
